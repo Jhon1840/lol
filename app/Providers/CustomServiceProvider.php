d4b86db923e65425adba5007d70e1e89596b8d8e
matias123lol <matiassbravo197@gmail.com>
2024-05-07 16:20:49 -0400

    railway_migrations db..

--- app/Providers/CustomServiceProvider.php
+++ app/Providers/CustomServiceProvider.php
@@ -13,7 +13,7 @@ class CustomServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // Aquí puedes registrar cualquier dependencia o servicio con el contenedor de servicios
     }
 
     /**
@@ -23,17 +23,28 @@ class CustomServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Coloca tu función aquí
-        function custom_array_flatten($array) {
-            $result = [];
-            foreach ($array as $item) {
-                if (is_array($item)) {
-                    $result = array_merge($result, custom_array_flatten($item));
-                } else {
-                    $result[] = $item;
-                }
+        // Puedes llamar a tu método personalizado aquí si es necesario en el arranque
+        $exampleArray = [[1, 2], [3, [4, 5]]];
+        $flattened = $this->customArrayFlatten($exampleArray);
+        // Puedes hacer algo con $flattened si es necesario
+    }
+
+    /**
+     * Aplana un array de manera recursiva.
+     * 
+     * @param array $array El array a aplanar.
+     * @return array El array aplanado.
+     */
+    private function customArrayFlatten($array)
+    {
+        $result = [];
+        foreach ($array as $item) {
+            if (is_array($item)) {
+                $result = array_merge($result, $this->customArrayFlatten($item));
+            } else {
+                $result[] = $item;
             }
-            return $result;
         }
+        return $result;
     }
 }
